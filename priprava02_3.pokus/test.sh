d92742f8a07963314dde9150bb2794cada38179a
Adam Vrana <adamvrana2342003@gmail.com>
2025-09-24 04:43:01 +0200

    update

--- priprava02_3.pokus/test.sh
+++ priprava02_3.pokus/test.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+echo "=== GENERATING TEST FILES ==="
+echo "Generating integers..."
+./gennum 5 10 > inty.txt
+
+echo "Generating floats..."
+./gennum 3 15 -f > float.txt
+
+echo "Generating hexadecimal numbers..."
+./gennum 5 8 -h > hexa.txt
+
+echo "Generating binary numbers..."
+./gennum 3 12 -b > bin.txt
+
+echo "Test files generated successfully!"
+echo
+echo "=== STARTING TESTS ==="
+echo
+
 # Změň do adresáře vernum
 cd vernum
 
